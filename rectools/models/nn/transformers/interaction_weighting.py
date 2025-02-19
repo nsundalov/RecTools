6ed49d8aefdd6f6f8fe47a404734c6b055187c9c
nsundalov1 <nsundalov1@mts.ru>
2025-02-19 11:04:22 +0300

    docs

--- rectools/models/nn/transformers/interaction_weighting.py
+++ rectools/models/nn/transformers/interaction_weighting.py
@@ -6,4 +6,14 @@ from torch import nn
 
 class InteractionWeightingBase(nn.Module):
     def forward(self, x: torch.Tensor, weights: torch.Tensor) -> torch.Tensor:
+        """
+        Base class for interaction weighting layers.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Embeddings of sessions. [batch_size, maxlen, n_factors]
+        weights : torch.Tensor
+            Weights for each item of session. [batch_size, maxlen]
+        """
         raise NotImplementedError()
